33c480f9321edd5cc68223b190e59e7f85fac1bd
Ed Ardolino <edwardardolino@gmail.com>
2021-09-09 07:03:33 -0400

    Added README, touched up CarDetect.py

--- CarDetect.py
+++ CarDetect.py
@@ -4,10 +4,11 @@ from tracker import *
 # Create tracker
 tracker = EuclideanDistTracker()
 
-#"/home/ed/Documents/Python/OpenCV/Car/highway.mp4"
-cap = cv2.VideoCapture("/home/ed/Documents/Python/OpenCV/Car/highway.mp4")
+#Video Source
+# If issues arise, paste the path of 'highway.mp4' into the quotes below
+cap = cv2.VideoCapture("highway.mp4")
 
-# Obj Detector
+# Object Detector
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
 while True:
@@ -15,11 +16,11 @@ while True:
     height, width, _ = frame.shape
 
 
-    # Extract and roi (Region of interest)
-    roi = frame[340: 720, 500: 800] 
+    # Extract and roi (Area of interest)
+    aoi = frame[340: 720, 500: 800] 
 
     # Object Detection
-    mask = object_detector.apply(roi)
+    mask = object_detector.apply(aoi)
     # 0 = black, 255, completly white
     # Threshold allows us to only consider objects that are completly white
     # Remove anything under 254
@@ -41,12 +42,12 @@ while True:
     for box_id in boxes_ids:
         x, y, w, h, id = box_id
 
-        cv2.putText(roi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.putText(aoi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.rectangle(aoi, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
 
-    cv2.imshow("roi", roi)
-    cv2.imshow("Frame", frame)
+    cv2.imshow("Area of Interst", aoi)
+    cv2.imshow("MP4", frame)
     cv2.imshow("Mask",mask)
 
     key = cv2.waitKey(30)
